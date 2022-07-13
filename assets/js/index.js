b6af7661524d44418181009b4efa4f4982aa247d
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-07-13 17:41:45 +0700

    fix sign up, dev sign in

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,48 @@
-const btnSignUp = () => {
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-  let new_user = {
+const getFormInput = () => {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const obj = {
     username: username,
     password: password,
   };
-  // checkUser(new_user);
-  addUser(new_user);
-  //sendNotify("success");
-  sendNotify("failed");
+  return obj;
 };
 
-// const checkUser = (new_user) => {
-//   // if (new_user.username === getUser(new_user.username)) {
-//   //   sendNotify("failed");
-//   // } else {
-//   //   addUser(new_user);
-//   //   sendNotify("success");
-//   // }
-//   if (!getUser(new_user)) {
-//     addUser(new_user);
-//     sendNotify("success");
-//   } else {
-//     sendNotify("failed");
-//   }
-// };
+const getUser = (key) => {
+  const user = localStorage.getItem(key);
+  return user;
+};
 
-// const getUser = (key) => {
-//   // let user = localStorage.getItem(key);
-//   // let userParse = JSON.parse(user);
-//   // return userParse.username;
-//   let exist_user = localStorage.getItem(key);
-//   let exist_user_parse = JSON.parse(exist_user);
-//   console.log(exist_user);
-//   console.log(exist_user_parse);
-//   if (key !== exist_user_parse.username) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
+const dataToString = (data) => {
+  return JSON.stringify(data);
+};
 
-const addUser = (data) => {
-  let dataNewUser = JSON.stringify(data);
-  localStorage.setItem(data.username, dataNewUser);
+const addUser = (new_user) => {
+  const new_user_parse = dataToString(new_user);
+  if (!getUser(new_user.username)) {
+    localStorage.setItem(new_user.username, new_user_parse);
+    sendNotify("success");
+    document.location.href = "../../sign_in/index.html";
+  } else {
+    sendNotify("failed");
+  }
+};
+
+const login = (user) => {
+  console.log(`${getUser(user.username)} \n ${user}`);
+  if (JSON.parse(getUser(user.username)) == user) {
+    sendNotify("success");
+  } else {
+    sendNotify("failed");
+  }
+};
+
+const btnSignUp = () => {
+  addUser(getFormInput());
+};
+
+const btnSignIn = () => {
+  login(getFormInput());
 };
 
 const Toast = Swal.mixin({
